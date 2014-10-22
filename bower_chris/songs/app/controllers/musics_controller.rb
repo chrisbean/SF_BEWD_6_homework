5f7ffedfde3fd729ec348f2ae5324642f384d268
chrisbean <christine@ga.co>
2014-10-21 22:20:39 -0700

    database of songs

---
+++ bower_chris/songs/app/controllers/musics_controller.rb
@@ -0,0 +1,24 @@
+class MusicsController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+end
